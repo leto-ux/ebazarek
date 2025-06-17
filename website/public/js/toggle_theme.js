5f82c3bd0a252ac03656e800e9e858133aae11cb
Daniel Stasielak <daniel.stasielak+it@icloud.com>
2025-06-17 22:12:32 +0200

    small steps

--- website/public/js/toggle_theme.js
+++ website/public/js/toggle_theme.js
@@ -2,16 +2,32 @@ $( document ).ready( function() {
     const $themeLink = $( '#theme_style' );
     const savedTheme = localStorage.getItem( 'theme' );
 
-    if( savedTheme ) {
-        $themeLink.attr( 'href', `/styles/style_${savedTheme}.css` );
+    const themes = {
+        light: "https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/flatly/bootstrap.min.css",
+        dark: "https://cdn.jsdelivr.net/npm/bootswatch@5.1.3/dist/darkly/bootstrap.min.css"
+    };
+
+    // Set saved motive
+    if( savedTheme && themes[ savedTheme ] ) {
+        $themeLink.attr( 'href', themes[ savedTheme ] );
+    } else {
+        // or default one ( dark )
+        $themeLink.attr( 'href', themes.dark );
+        localStorage.setItem( 'theme', 'dark' );
     }
 
     $( '#toggle_theme' ).on( 'click', function () {
-        const currentHref = $themeLink.attr( 'href' );
-        const currentTheme = currentHref.includes( 'dark' ) ? 'dark' : 'light';
-        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+        // const currentTheme = localStorage.getItem( 'theme' ) === 'dark' ? 'dark' : 'light';
+        // const newTheme = currentTheme === 'light' ? 'dark' : 'light';
 
-        $themeLink.attr( 'href', `/styles/style_${newTheme}.css` );
-        localStorage.setItem( 'theme', newTheme );
+        // $themeLink.attr( 'href', themes[ newTheme ]);
+        // localStorage.setItem( 'theme', newTheme );
+        //
+        const currentTheme = localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+        $themeLink.attr('href', themes[newTheme]);
+        localStorage.setItem('theme', newTheme);
+        console.log( "Wykonano " );
     });
 });
