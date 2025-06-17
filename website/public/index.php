ad88436e974a03d26ed7e9e1a6c6f88da68a8d96
Daniel Stasielak <daniel.stasielak+it@icloud.com>
2025-06-17 13:05:14 +0200

    IT WOOOOORKSKSSSSS

--- website/public/index.php
+++ website/public/index.php
@@ -1,11 +1,10 @@
 <?php
     require_once __DIR__ . '/../vendor/autoload.php';
 
-    $loader = new \Twig\Loader\Filesystem( __DIR__ . '/../templates' );
+    $loader = new \Twig\Loader\FilesystemLoader( __DIR__ . '/templates' );
     $twig = new \Twig\Environment( $loader );
 
     echo $twig -> render( 'index.html', [
         'foo' => 'bar'
     ]);
-
 ?>
