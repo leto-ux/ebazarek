648ace0b7d9d5f116749988e3bbf8de2fa65ac32
Daniel Stasielak <daniel.stasielak+it@icloud.com>
2025-06-30 18:10:29 +0200

    let's see if this works for rusty man

--- website/public/finalize.php
+++ website/public/finalize.php
@@ -1,5 +1,31 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if (!defined('IN_INDEX')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+    include( 'config.php' );
+    include( 'helpers.php' );
+}
 
-var_dump( $_POST );
+if( !isset($_POST['status']) && !isset($_POST['offer_id']) &&
+    ( !isset($_POST['delivery_info']) || $_POST['status'] === 'abandoned' )){
+    print TwigHelper::getInstance() -> render( 'error.html', []);
+    exit();
+}
+
+if( $_POST['status'] === 'paid' ){
+    echo "<br><p>Zakupiono</p>";
+} else {
+    $stmt = DB::getInstance()->prepare("
+        UPDATE Offers
+        SET status = 'active'
+        WHERE OfferID = ?
+        AND status = 'pending'
+    ");
+    $stmt->execute([$_POST['offer_id']]);
+    header( 'Location: /offers' );
+    exit();
+}
 
 ?>
